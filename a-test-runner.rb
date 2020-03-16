977108c8c8ec38197b6a9b3692336510b9279483
Kelly Redding <kelly@kellyredding.com>
2020-03-16 10:48:22 -0500

    version to 1.0.1
    * update to CLIRB 1.1.0 955c15a
    * rework to not require debug mode regex handling #2

--- a-test-runner.rb
+++ a-test-runner.rb
@@ -5,7 +5,7 @@ require "set"
 
 # https://github.com/redding/a-test-runner.rb
 module ATestRunner
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 
   # update these as needed for your test setup
   BIN_NAME           = "a-test-runner"
